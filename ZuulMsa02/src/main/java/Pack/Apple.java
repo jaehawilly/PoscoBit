c3202ecc2aef64125829c1781f48b4d194be9f87
Jaeha Lee <37172636+jaehawilly@users.noreply.github.com>
2023-01-11 16:08:13 +0900

    zuul
    zuul

--- ZuulMsa02/src/main/java/Pack/Apple.java
+++ ZuulMsa02/src/main/java/Pack/Apple.java
@@ -3,6 +3,7 @@ package Pack;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,10 +27,11 @@ public class Apple {
 }
 
 @RestController
-@RequestMapping("/app")
-class AppController{
-	@GetMapping("/{id}")
-	public String f1(@PathVariable Integer id) {
-		return (id==1000) ? "로그인성공" : "로그인실패";
+@RequestMapping("/bpp")
+class BppController{
+	@GetMapping("/{token}")
+	public String f1(@PathVariable Integer token) {
+		System.out.println("bpp/" + token);
+		return "재하 " + token;
 	}
 }
