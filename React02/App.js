b14c2df85f6024d46a3d43ef78fa5088760bfad0
Jaeha Lee <37172636+jaehawilly@users.noreply.github.com>
2023-01-17 11:33:44 +0900

    iOS React Native 성공
    iOS React Native 성공

---
+++ React02/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import RootStack from './screens/RootStack';
+import {LogContextProvider} from './contexts/LogContext';
+import {SearchContextProvider} from './contexts/SearchContext';
+import 'react-native-get-random-values';
+
+function App() {
+  return (
+    <NavigationContainer>
+      <SearchContextProvider>
+        <LogContextProvider>
+          <RootStack />
+        </LogContextProvider>
+      </SearchContextProvider>
+    </NavigationContainer>
+  );
+}
+
+export default App;
